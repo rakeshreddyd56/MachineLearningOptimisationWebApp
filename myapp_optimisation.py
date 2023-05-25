43603609ff461d773c36e26dbbd2b3ded1880809
Rakeshreddyd56 <58788206+rakeshreddyd56@users.noreply.github.com>
2023-05-25 14:38:58 -0500

    Update myapp_optimisation.py

--- myapp_optimisation.py
+++ myapp_optimisation.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import base64
-pip install plotly
 import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
